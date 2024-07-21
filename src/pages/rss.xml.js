47658fe112d2ee64ef4174568226822a045ede82
BcRikko <s.shin1106@gmail.com>
2024-07-21 20:24:01 +0900

    add rss

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,12 @@
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+
+export async function GET(context) {
+  return rss({
+    title: 'Today I learned. | ダーシノ(@bc_rikko)',
+    description: 'フロントエンド関連で今日学んだことを共有',
+    site: context.site,
+    items: await pagesGlobToRssItems(
+      import.meta.glob('./posts/**/*.{md,mdx}'),
+    ),
+  })
+}
