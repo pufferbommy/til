4ab8cde56fea526dfbd6d7c91533fce424207b24
BcRikko <s.shin1106@gmail.com>
2024-07-21 17:23:19 +0900

    テンプレート修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://bcrikko.github.io/til',
-  base: '/til'
+  site: 'https://bcrikko.github.io',
+  base: '/til',
+  integrations: [mdx()]
 });
