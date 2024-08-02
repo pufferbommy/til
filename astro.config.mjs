d797ea7dabc3b5384f85eb00e40bd57cc740f880
BcRikko <s.shin1106@gmail.com>
2024-08-03 00:20:31 +0900

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bcrikko.github.io',
   base: '/til',
-  integrations: [mdx()]
+  integrations: [
+    mdx(),
+    sitemap(),
+  ]
 });
