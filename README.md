1b32feaf33982a685b79b6bee260c6922adec4da
pufferbommy <pufferbommy@gmail.com>
2025-01-25 00:06:08 +0700

    add posts

--- README.md
+++
@@ -1,8 +0,0 @@
-# Today I learned.
-
-私が今日学んだことを共有するためのリポジトリです。主にフロントエンド関連のニュースが中心になると思います。  
-This is a repository for sharing what I learned today. It will mainly focus on front-end related news.
-
-
-[購読](https://bcrikko.github.io/til/rss)からフィードを購読できます。  
-You can subscribe to the feed from [subscription](https://bcrikko.github.io/til/rss).
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,8 +2,8 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context) {
   return rss({
-    title: 'Today I learned. | ダーシノ(@bc_rikko)',
-    description: 'フロントエンド関連で今日学んだことを共有',
+    title: 'Today I learned. | pufferbommy',
+    description: 'Sharing what I learned today',
     site: context.site,
     trailingSlash: true,
     items: await pagesGlobToRssItems(
