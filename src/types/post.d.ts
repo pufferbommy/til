3cbb54ed5b59924cc2a206822d486b27160802fa
pufferbommy <pufferbommy@gmail.com>
2025-01-25 13:56:40 +0700

    fix errors

--- src/types/post.d.ts
+++ src/types/post.d.ts
@@ -1,7 +1,7 @@
-declare type MyPost = {
-  pubDate: string
-  updatedDate: string
+declare type PostFrontmatter = {
   title: string
   description: string
+  pubDate: string
+  updatedDate: string
   tags: string[]
 }
